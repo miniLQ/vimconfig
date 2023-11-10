b3bebf59e01c60cf8eca2fe9272ef08b65dab0af
liuqi <liuqi@longcheer.com>
2023-11-10 10:54:08 +0800

    add tools
    Signed-off-by: liuqi <liuqi@longcheer.com>

---
+++ offline_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: liuqi
+# Created Time : Mon 23 Oct 2023 10:08:49 AM CST
+# File Name: offline_install.sh
+# Description:
+
+echo "start offline_install"
+cp -rf ./vim $HOME/.vim
+cp ./vimrc $HOME/.vimrc
+echo "endo offline_install"
